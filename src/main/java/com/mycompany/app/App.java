cebda56b675fff25b7bf5a7848f7a27f6cdd12b8
Jhon Baron <32346414+Jhooomn@users.noreply.github.com>
2020-07-02 10:19:28 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
     private final String message = "Hello World!";
-
+    // Testing CI
     public App() {}
 
     public static void main(String[] args) {
